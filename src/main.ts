6e3a27d87b8ac1c99ade2e1f29c0023374a97999
cbcristhian <cristhian.castro@unosquare.com>
2024-06-08 11:04:39 -0400

    orders ms connected to gateway

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { envs } from './config/env';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(AppModule,{
+    transport:Transport.TCP,
+    options:{
+      port:envs.port
+    }
+  });
+  await app.listen();
 }
 bootstrap();
